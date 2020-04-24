57c53c0a596d2040ca7a1c8a7d0a1c2454ff386b
FatElefant <63770994+FatElefant@users.noreply.github.com>
2020-04-24 14:27:29 +0700

    Includes colour boxes in index

---
+++ GammaGamma_Scripts_index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+  let firstblock = document.createElement('h1');
+  document.body.appendChild(firstblock);
+  firstblock.id='firstblock'
+  firstblock.innerHTML='This text is primary colour and the background is secondary colour.'
+  let secblock = document.createElement('h1');
+  document.body.appendChild(secblock);
+  secblock.id='secblock'
+  secblock.innerHTML='This text is tertiary colour and the background is quarternary colour.'
+  let thirdblock = document.createElement('h1');
+  document.body.appendChild(thirdblock);
+  thirdblock.id='thirdblock'
+  thirdblock.innerHTML='This text is secondary colour and the background is primary colour.'
+  let fourthblock = document.createElement('h1');
+  document.body.appendChild(fourthblock);
+  fourthblock.id='fourthblock'
+  fourthblock.innerHTML='This text is tertiary colour and the background is primary colour.'
+})
